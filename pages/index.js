155de4f8694718f435aec2c61be7cc197eb41698
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-25 19:31:50 -0700

    Added a NavBar component

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet"></link>
     </head>
     <body class="bg-black">
-    <div class="flex place-content-center">
+    <div class="flex place-content-center my-10">
       <h1 class="text-6xl font-permanent-marker text-white">Paolo van Rijn</h1>
     </div>
     </body>
