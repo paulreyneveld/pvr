10764f065dbb205118468103efb50c026e4a4adf
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-25 19:17:33 -0700

    Updated Tailwind's config to support the use of a Google Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-     extend: {},
+     extend: {
+       fontFamily: {
+         'permanent-marker': ['Permanent Marker']
+       }
+     },
   },
   variants: {
     extend: {},
