4fd8430618bab12ed96737e54a60dcccfbb41be3
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-25 19:15:18 -0700

    Cleared error

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,16 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
-    <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <>
+    <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
       <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet"></link>
-    </Head>
+    </head>
+
+    <div>
     <p>Home</p>
     </div>
+    </>
   )
 }
