7fffc6afcd36e4f0fbdcecd714cb0e373cbdd7b0
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-25 19:33:10 -0700

    Populated NavBar

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,19 @@
 import Link from 'next/link'
 
 const NavBar = () => {
-    
+    return (
+        <>
+        <Link>
+            <a>Home</a>
+        </Link>
+        <Link>
+            <a>Music</a>
+        </Link>
+        <Link>
+            <a>Merch</a>
+        </Link>
+        </>
+    )
 }
+
+export default NavBar;
