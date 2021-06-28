605fc47e6d28634e83efa05e8114e65fe0bacb9e
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-28 14:22:32 -0700

    Added some meaty lorem

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import Head from 'next/head';
 import Layout from '../components/Layout';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+
+  const [lorem, setLorem] = useState();
+
+  const getData = async () => {
+    const response = await axios.get('https://baconipsum.com/api/?type=meat-and-filler&paras=5&format=text');
+    setLorem(response.data);
+  }
+
+  useEffect(() => getData(), []);
+
+  console.log(lorem);
+
   return (
-    <>
-      <p class="text-white">testing</p>
-    </>
+    <div className="container mx-auto">
+      <p className="text-white">{lorem}</p>
+    </div>
   )
 }
