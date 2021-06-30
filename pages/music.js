8fa0bbc65d716d5b4ba51940760ac988a12f1c3c
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-30 14:53:46 -0700

    Embedded a Soundcloud file into the music page

--- pages/music.js
+++ pages/music.js
@@ -1,4 +1,5 @@
 import Layout from '../components/Layout';
+import ReactPlayer from 'react-player';
 
 const Music = () => {
 
@@ -6,7 +7,9 @@ const Music = () => {
     return (
         <div class="flex place-content-center">
         <p class="text-white block">Old Track</p>
-
+        <ReactPlayer 
+            url="https://soundcloud.com/user-680027355/foreign-currencies"
+        />
         </div>
     )
 }
