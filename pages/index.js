45f24515dbb038cf4799b80bd2904d5338dfbda5
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-06-29 18:14:36 -0700

    Added margins to the main content

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
 
   return (
     <div className="container mx-auto">
-      <p className="text-white">{lorem}</p>
+      <p className="mr-8 ml-8 text-white">{lorem}</p>
     </div>
   )
 }
