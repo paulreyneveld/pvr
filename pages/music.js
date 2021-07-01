daf45ab9ae4a45f8b4b77a584df995dc072d2b1d
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-07-01 16:10:43 -0700

    Added new links

--- pages/music.js
+++ pages/music.js
@@ -6,7 +6,19 @@ const Music = () => {
     return (
         <div class="flex place-content-center">
         <ReactPlayer 
-            height="100px"
+            height="150px"
+            url="https://soundcloud.com/user-680027355/walk-breathe-look"
+        />
+        <ReactPlayer 
+            height="150px"
+            url="https://soundcloud.com/user-680027355/underneath-the-sheets-as-lullaby"
+        />
+        <ReactPlayer 
+            height="150px"
+            url="https://soundcloud.com/user-559390371/giacobinids"
+        />
+        <ReactPlayer 
+            height="150px"
             url="https://soundcloud.com/user-680027355/foreign-currencies"
         />
         </div>
