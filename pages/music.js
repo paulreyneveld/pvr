dfaa9b3d410d1127361e3ca1c8598643667ce4b8
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-07-01 16:26:22 -0700

    Updated music page and changed the css to improve the appearance

--- pages/music.js
+++ pages/music.js
@@ -4,24 +4,34 @@ import ReactPlayer from 'react-player';
 const Music = () => {
 
     return (
-        <div class="flex place-content-center">
+        
+        <div class="grid grid-cols-1 gap-10 place-items-center">
+        <div>
         <ReactPlayer 
             height="150px"
             url="https://soundcloud.com/user-680027355/walk-breathe-look"
         />
+        </div>
+        <div>
         <ReactPlayer 
             height="150px"
             url="https://soundcloud.com/user-680027355/underneath-the-sheets-as-lullaby"
         />
+        </div>
+        <div>
         <ReactPlayer 
             height="150px"
             url="https://soundcloud.com/user-559390371/giacobinids"
         />
+        </div>
+        <div>
         <ReactPlayer 
             height="150px"
-            url="https://soundcloud.com/user-680027355/foreign-currencies"
+            url="https://soundcloud.com/user-680027355/did-you-miss-me-too"
         />
         </div>
+        </div>
+        
     )
 }
 
